38f95aba5301666da5a2670b86409eb1a6179f6f
ankush953 <ankushraghuvanshi07@gmail.com>
2020-08-15 12:32:19 +0530

    path-sum-iii medium

---
+++ path-sum-iii.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+
+public:
+    int pathSum(TreeNode *root, int sum, vector<int> curr = {})
+    {
+        if (!root)
+            return 0;
+        int ans = 0;
+        int temp = sum;
+        curr.push_back(root->val);
+        for (int i = curr.size() - 1; i > -1; i--)
+        {
+            temp -= curr[i];
+            ans += (temp == 0);
+        }
+        ans += pathSum(root->left, sum, curr);
+        ans += pathSum(root->right, sum, curr);
+        curr.pop_back();
+        return ans;
+    }
+};
