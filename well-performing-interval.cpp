451711250cc95a3f1f7e81dd23cc70c28b1d11cc
ankush953 <ankushraghuvanshi07@gmail.com>
2020-08-19 21:40:51 +0530

    well-performing-interval - medium

--- well-performing-interval.cpp
+++ well-performing-interval.cpp
@@ -2,19 +2,28 @@
 // space O(n)
 // time O(n)
 
-class Solution {
+class Solution
+{
 public:
-    int longestWPI(vector<int>& hours) {
-        unordered_map<int,int>seenBefore;
+    int longestWPI(vector<int> &hours)
+    {
+        unordered_map<int, int> seenBefore;
         int sum = 0, ans = 0;
-        for(int i = 0;i<hours.size();i++){
-            sum += (hours[i] > 8 ? 1: -1);
-            if(sum > 0){
-                ans = i+1;
-            }else{
-                if(seenBefore.find(sum-1) != seenBefore.end()){
-                    ans = max(ans, i - seenBefore[sum-1]);
-                }else if(seenBefore.find(sum) == seenBefore.end()){
+        for (int i = 0; i < hours.size(); i++)
+        {
+            sum += (hours[i] > 8 ? 1 : -1);
+            if (sum > 0)
+            {
+                ans = i + 1;
+            }
+            else
+            {
+                if (seenBefore.find(sum - 1) != seenBefore.end())
+                {
+                    ans = max(ans, i - seenBefore[sum - 1]);
+                }
+                else if (seenBefore.find(sum) == seenBefore.end())
+                {
                     seenBefore[sum] = i;
                 }
             }
