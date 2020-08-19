3f20ee3d0f8fbe6e472ddd3001a7acc71d452b08
ankush953 <ankushraghuvanshi07@gmail.com>
2020-08-19 13:23:37 +0530

    well-performing-interval - medium

---
+++ well-performing-interval.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/longest-well-performing-interval/
+// space O(n)
+// time O(n)
+
+class Solution {
+public:
+    int longestWPI(vector<int>& hours) {
+        unordered_map<int,int>seenBefore;
+        int sum = 0, ans = 0;
+        for(int i = 0;i<hours.size();i++){
+            sum += (hours[i] > 8 ? 1: -1);
+            if(sum > 0){
+                ans = i+1;
+            }else{
+                if(seenBefore.find(sum-1) != seenBefore.end()){
+                    ans = max(ans, i - seenBefore[sum-1]);
+                }else if(seenBefore.find(sum) == seenBefore.end()){
+                    seenBefore[sum] = i;
+                }
+            }
+        }
+        return ans;
+    }
+};
